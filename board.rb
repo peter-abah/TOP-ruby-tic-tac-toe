447f8dcf50b92906a95957a1761c2821fc13c095
Peter Abah <peterabah.ob@gmail.com>
2021-05-09 13:31:14 +0100

    create update method in Board class

--- board.rb
+++ board.rb
@@ -2,4 +2,10 @@ class Board
   def initialize
     @board = Array.new(3) { Array.new(3) }
   end
+
+  def update(move, player)
+    board = @board.map(&:dup)
+    board[move[0]][move[1]] = player
+    new_board(board)
+  end
 end
