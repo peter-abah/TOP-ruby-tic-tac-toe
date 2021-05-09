475b40fce11cc04c9be54d0eb1dfeebbf64de68f
Peter Abah <peterabah.ob@gmail.com>
2021-05-09 12:35:00 +0100

    Create start method

--- game.rb
+++ game.rb
@@ -1,8 +1,24 @@
 class Game
-  def initialize()
+  def initialize
     @players = %w(X 0)
     @board = Board.new
   end
 
-  def 
+  def start
+    turn = 0
+    game_end = false
+    display_board
+    until game_end
+      current_player = @players[turn]
+      move = get_move
+      @board = @board.update(move, current_player)
+      if @board.win?
+        puts "${current_player} won"
+        game_end = true
+      elsif @board.draw?
+        puts 'DRAW!'
+        game_end = true
+      end
+    end
+  end
 end
