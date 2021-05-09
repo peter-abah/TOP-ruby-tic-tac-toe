51e4911a03e7d11c0856ec9d3171365fbe5c6cd8
Peter Abah <peterabah.ob@gmail.com>
2021-05-09 14:53:51 +0100

    Add win? method to Board

--- board.rb
+++ board.rb
@@ -1,8 +1,8 @@
 class Board
-  BOARD_COMBINATIONS = 
-      [ [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]]
+  BOARD_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]]
 
   def initialize(board = Array.new(9))
     @board = board
@@ -16,5 +16,11 @@ class Board
 
   def played?(move)
     @board[move] ? true : false
+  end
+
+  def win?(player)
+    BOARD_COMBINATIONS.some do |c|
+      c.all { |i| @board[i] == player}
+    end
   end
 end
