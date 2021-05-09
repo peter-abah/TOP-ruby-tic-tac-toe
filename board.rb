8432c9b535742ed574472daeb64e7d5130ce67c4
Peter Abah <peterabah.ob@gmail.com>
2021-05-09 15:00:04 +0100

    Add draw? method to Board

--- board.rb
+++ board.rb
@@ -2,7 +2,8 @@ class Board
   BOARD_COMBINATIONS = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]]
+    [0, 4, 8], [2, 4, 6]
+  ].freeze
 
   def initialize(board = Array.new(9))
     @board = board
@@ -20,7 +21,13 @@ class Board
 
   def win?(player)
     BOARD_COMBINATIONS.some do |c|
-      c.all { |i| @board[i] == player}
+      c.all { |i| @board[i] == player }
+    end
+  end
+
+  def draw?
+    BOARD_COMBINATIONS.all do |c|
+      c.all { |i| @board[i].nil? }
     end
   end
 end
