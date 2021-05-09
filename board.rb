ebde803e27ab7eb094042a182ccbed6b15423a6a
Peter Abah <peterabah.ob@gmail.com>
2021-05-09 13:47:00 +0100

    Update initialize method to receive an optional board argument

--- board.rb
+++ board.rb
@@ -1,11 +1,11 @@
 class Board
-  def initialize
-    @board = Array.new(3) { Array.new(3) }
+  def initialize(board = Array.new(3) { Array.new(3) })
+    @board = board
   end
 
   def update(move, player)
     board = @board.map(&:dup)
     board[move[0]][move[1]] = player
-    new_board(board)
+    Board.new(board)
   end
 end
